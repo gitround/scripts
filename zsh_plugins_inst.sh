a4f031403bdfba905cb1e598067fb88a2c1c3f15
gitround <87219844+gitround@users.noreply.github.com>
2021-07-30 06:03:47 -0700

    Create zsh_plugins_inst.sh

---
+++ zsh_plugins_inst.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## Installs all the zsh plugins i use with oh-my-zsh in .zshrc
+git clone https://github.com/supercrabtree/k $ZSH_CUSTOM/plugins/k
+
+
+
+
+
+
+
+#### Icons in Terminal
+# https://github.com/Yash-Handa/logo-ls/releases/download/v1.3.7/logo-ls_Linux_x86_64.tar.gz
+# extract logo-ls_Linux_x86_64.tar.gz
+# dpkg -i logo-ls
+#### IF the last cmd doesnt work
+# sudo mv logo-ls /bin
